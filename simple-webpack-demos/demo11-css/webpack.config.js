fc3db40764331a3abedad5da09bfe2bf81f7a66b
xuyadong <xuyadong@qiyi.com>
2019-09-08 16:59:44 +0800

    webpack split css

--- simple-webpack-demos/demo11-css/webpack.config.js
+++ simple-webpack-demos/demo11-css/webpack.config.js
@@ -1,14 +1,60 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-  mode: 'development',
+  mode: 'production',
   entry: {
-    src: './src/index.js'
+    app: './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js'
+    filename: '[id].[name].bundle.js',
+    chunkFilename: '[id].[name].chunk.js'
   },
+  module: {
+  	rules: [
+  		{
+  			test: /\.css$/,
+  			use: [MiniCssExtractPlugin.loader, 'css-loader'],
+  		}
+  	]
+  },
+  optimization: {
+  	runtimeChunk: 'single',
+  	splitChunks: {
+  		cacheGroups: {
+  			async: {
+          name: 'async',
+          // test: /\.js$/,
+          // enforce: true,
+          chunks: 'async',
+          minChunks: 1,
+          minSize: 0
+        },
+  			vendors: {
+          test: /[\\/]vendor[\\/].*\.js$/,
+          name: 'vendor',
+          chunks: 'all',
+          minChunks: 1,
+          minSize: 0
+        },
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          // enforce: true,
+          minChunks: 1,
+          minSize: 0
+        },
+  		}
+  	}
+  },
+  plugins: [
+  	new MiniCssExtractPlugin({
+  		filename: '[id].[name].[chunkhash:8].css',
+  		chunkFilename: '[id].[name].[chunkhash:8].css',
+  	})
+  ]
 };
 
 module.exports = config;
